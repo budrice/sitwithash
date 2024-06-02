bd2995e9588dfdcc52e3b5e3d230503b0eb3e91d
Buddy <39768980+budrice@users.noreply.github.com>
2024-06-02 12:41:34 -0400

    masthead
    created masthead component for header

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MastheadComponent } from "./masthead/masthead.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, MastheadComponent]
 })
 export class AppComponent {
   

d261e45757d4c476e0d2a7dc63c3e01f69e68974
Buddy <39768980+budrice@users.noreply.github.com>
2024-06-04 16:23:38 -0400

    navbar component
    navbar component created

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MastheadComponent } from "./masthead/masthead.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, MastheadComponent]
+    imports: [RouterOutlet, MastheadComponent, NavbarComponent]
 })
 export class AppComponent {
   year = new Date().getFullYear();
